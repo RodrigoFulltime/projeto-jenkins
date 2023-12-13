750705a070aa299013537030b31257f28513067b
RodrigoFulltime <134294178+RodrigoFulltime@users.noreply.github.com>
2023-12-13 08:56:35 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function resposta(acao) {
-    if (acao === 'nao') {
-      let randomLeft = Math.floor(Math.random() * 80);
-      document.getElementById('nao-btn').style.left = `${randomLeft}%`;
-    } else {
-      alert('Que bom! Vamos namorar!');
-    }
-  }
-  
